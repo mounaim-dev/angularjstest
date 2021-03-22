4ae0cf979fc345f807930dd4bcfe11099a277d2e
mounaim-dev <80955158+mounaim-dev@users.noreply.github.com>
2021-03-22 04:47:48 +0100

    update app.js

--- js/app.js
+++ js/app.js
@@ -2,17 +2,7 @@
 
 var myApp = angular.module("myApp", ['angularUtils.directives.dirPagination']);
 
-// myApp.controller('MainCtrl', function ( $scope, myService ) {
-//     $scope.loading = true;
-//     myService.get().then( function ( response ) {
-//       $scope.items = response.data;
-//     }, function ( response ) {
-//       // TODO: handle the error somehow
-//     }).finally(function() {
-//       // called no matter success or failure
-//       $scope.loading = false;
-//     });
-//   });
+
 
 
 
